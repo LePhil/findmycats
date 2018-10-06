18d842b97fdc9e9a7397cb7be08b948b410debd1
Phil <philipp.christen@gmail.com>
2018-10-06 17:04:25 +0200

    using express, testing env vars

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+let express = require("express");
+let request = require('request');
+let app = express();
+
+app.listen((process.env.PORT || 3000));
+
+app.get('/', function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+
+app.get('/test', function (req, res) {
+    res.send(process.env.test || "nope");
 });
